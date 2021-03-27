316cd5353e2827db83f5b0aecd02dae0d0645908
Vishnu123i <vishnume06@gmail.com>
2021-03-27 17:43:15 +0530

    Task Completed

--- Vishnu/SQL Practice.sql
+++ Vishnu/SQL Practice.sql
@@ -1,8 +1,30 @@
 show databases;
+
 create database customerdb;
 use customerdb;
-drop database customerdb;
-create table Employee(Name varchar(20), D.O.B. date, Salary number(6);
-create table customer(id int(5),name varchar(50),email varchar(50),phone nvarchar(13));
-show tables;
-describe customer;
+
+create table customers(id int, first_name varchar(50),last_name varchar(50),email varchar(50),city varchar(50));
+insert into customers values(3,'srikanth','reddy','srikanth@gmail.com','Hyderabad');
+insert into customers values(1,'Vishnu','Reddy','vishnu@gmail.com','Guntur');
+insert into customers values(2,'satyanarayana','chilakala','satya@gmail.com','Piduguralla');
+insert into customers (id,first_name,city)values(4,'harsha','banglore');
+select * from customers;
+select * from customers where first_name='vishnu';
+select * from customers where first_name='vishnu'and email='vishnu@gmail.com';
+select * from customers where first_name='srikanth'or city='Piduguralla';
+select * from customers where id in (1,2);
+delete from customers where first_name='harsha';
+delete from customers where id=3;
+delete from customers where last_name='Reddy';
+delete from customers where first_name='satyanarayana';
+update customers set last_name='sri' where city='banglore';
+update customers set email='harsha@gmail.com' where last_name='sri';
+select * from customers where last_name like '%y';
+select first_name from customers where id BETWEEN 1 AND 2;
+select * from customers where id =2;
+select * from customers where id !=2;
+select * from customers where id <=2;
+select * from customers where id >=2;
+select * from customers where id >2;
+select * from customers where id <2;
+
